d2fe4233e25ed06c36de37c45ffa8836e6f1160b
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-23 03:31:27 +0200

    add login, signup pages

---
+++ pages/login/index.js
@@ -0,0 +1,12 @@
+import Login from '@/components/Login/Login';
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            <Login></Login>
+        </div>
+    );
+};
+
+export default index;
---
+++ pages/signup/index.js
@@ -0,0 +1,12 @@
+import Signup from '@/components/signup/Signup';
+import React from 'react';
+
+const index = () => {
+    return (
+        <div>
+            <Signup></Signup>
+        </div>
+    );
+};
+
+export default index;
