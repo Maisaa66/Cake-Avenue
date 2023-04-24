6823d6d4558284eec99f971001084730b2eb9c5a
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-24 12:20:52 +0200

    create page for create new recipe post

---
+++ pages/recipes/createPost.js
@@ -0,0 +1,12 @@
+import PostCard from '@/components/Post/PostCard';
+import React from 'react';
+
+const createPost = () => {
+    return (
+        <div>
+            <PostCard></PostCard>
+        </div>
+    );
+};
+
+export default createPost;
