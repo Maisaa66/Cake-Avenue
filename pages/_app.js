4bf73907e16e055120af33046be5f5417c8dca41
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-25 02:06:09 +0200

    Added GitHub authentication using OAuth

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,22 @@ import Navbar from '@/components/Navbar/Navbar'
 import "../styles/globals.css";
 // import "../node_modules/mdb-ui-kit/css/mdb.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { SessionProvider } from "next-auth/react";
 
 // import "../node_modules/mdb-ui-kit/js/mdb.min";
 import { useEffect } from 'react';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component,   pageProps: { session, ...pageProps }, }) {
   useEffect(()=>{
     import("../node_modules/bootstrap/dist/js/bootstrap.min.js")
   },[])
 
   return (
     <>
-    <Navbar></Navbar>
+  <SessionProvider session={session}>
+  <Navbar></Navbar>
     <Component {...pageProps} />
+  </SessionProvider>
     </>
   )
 }
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth/next";
+import GithubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+    providers: [
+        GithubProvider({
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECERT,
+        }),
+    ],
+});
+
+
