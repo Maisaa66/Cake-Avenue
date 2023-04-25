4e7ebc9fab58355e4f52fedc83952c5868ddf129
Maisaa Hamza <maisaa.hamza66@gmail.com>
2023-04-25 09:30:09 +0200

    add Facebook authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,16 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
+import FacebookProvider from "next-auth/providers/facebook";
 
 export default NextAuth({
     providers: [
         GithubProvider({
             clientId: process.env.CLIENT_ID,
             clientSecret: process.env.CLIENT_SECERT,
+        }),
+        FacebookProvider({
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET,
         }),
     ],
 });
